f259178bbca36b93bb0760755140e43bc0ed693f
mpuchan <iputumpu23@gmail.com>
2019-08-27 09:44:08 +0800

    [ADD] link show data Boards and Cards

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,38 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+user= User.create{
+  email: "mpuchan@gmail.com"
+  password: "wasdasw"
+  password_cc: "wasdasw"
+}
+
+board = Board.create{
+  title: 'PKL'
+  description: "Board tentang aktivitas PKL Jul -Okt 2019",
+  user_id: user.id
+}
+
+Card.create([
+  {
+    title: 'Neque porrro quisquam',
+    content: "Lorem ipsum"
+    user_id: user.id,
+    board_id: board.id,
+    status: 0
+  },
+  {
+    title: 'Neque porrro quisquam',
+    content: "Lorem ipsum"
+    user_id: user.id,
+    board_id: board.id,
+    status: 0
+  },
+  {
+    title: 'Neque porrro quisquam',
+    content: "Lorem ipsum"
+    user_id: user.id,
+    board_id: board.id,
+    status: 0
+
+  },
+
+
+])
