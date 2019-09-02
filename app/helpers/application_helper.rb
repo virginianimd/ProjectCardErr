c386ab2bc1a6e46219a42ec13f38b5ab0ab89b4c
mpuchan <iputumpu23@gmail.com>
2019-09-02 09:20:09 +0800

    [FIX]  rule of code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
-    def user_avatar(user, size=40)
-        if user.avatar.attached?
-          user.avatar.variant(resize: "#{size}x#{size}!")
-        else
-          gravatar_image_url(user.email, size: size)
-        end
-      end
+  def user_avatar(user, size=40)
+    if user.avatar.attached?
+      user.avatar.variant(resize: "#{size}x#{size}!")
+      else
+        gravatar_image_url(user.email, size: size)
+    end
+  end
 end
